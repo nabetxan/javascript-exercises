6ca398dcde043b900bca6fadb6c038ae716145e9
nabetxan <119922401+nabetxan@users.noreply.github.com>
2023-02-23 21:01:05 +0900

    js exercise 02 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function (string, times) {
+  let repeatedString = "";
 
+  if (times < 0) {
+    return "ERROR";
+  }
+
+  for (let i = 1; i <= times; i++) {
+    repeatedString = repeatedString + string;
+  }
+  return repeatedString;
 };
+
+repeatString("mino", 4);
 
 // Do not edit below this line
 module.exports = repeatString;
