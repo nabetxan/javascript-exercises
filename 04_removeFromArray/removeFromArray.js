18826e304fce44c678f31da1fb97e39db90e88bb
nabetxan <119922401+nabetxan@users.noreply.github.com>
2023-02-25 08:41:20 +0900

    js exercise 04 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...removeValue) {
+  const newArray = [...array];
+  for (let i = 0; i < removeValue.length; i++) {
+    const index = newArray.indexOf(removeValue[i]);
+    if (index !== -1) {
+      newArray.splice(index, 1);
+    }
+  }
+  return newArray;
 };
 
 // Do not edit below this line
