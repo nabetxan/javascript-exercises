e2708854ac98995bcd616df8f0fe9df90efcbcbb
nabetxan <119922401+nabetxan@users.noreply.github.com>
2023-03-01 16:38:36 +0900

    js exercise 07 completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,22 @@
-const convertToCelsius = function() {
+const roundToOneDecimal = function (num) {
+  return Math.round(num * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function (temp) {
+  //(temp-32) * 5/9
+
+  let celsius = ((temp - 32) * 5) / 9;
+  return roundToOneDecimal(celsius);
+};
+
+const convertToFahrenheit = function (temp) {
+  //(temp * 9/5) + 32
+  let fahrenheit = (temp * 9) / 5 + 32;
+  return roundToOneDecimal(fahrenheit);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
