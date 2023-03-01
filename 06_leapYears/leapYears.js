c12230b7bdd97df9329d02ede0dc46cc751ad4e8
nabetxan <119922401+nabetxan@users.noreply.github.com>
2023-03-01 16:37:54 +0900

    js exercise 06 completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  // can't be divided by 4? >> false
+  if (year % 4 !== 0) {
+    return false;
+  }
 
+  // can be devided by 100 && can't be divided by 400? >> false
+  if (year % 100 === 0 && year % 400 !== 0) {
+    return false;
+  }
+
+  return true;
 };
 
 // Do not edit below this line
