173cff726f0c502a0bc4f1b17266e45704a565da
nabetxan <119922401+nabetxan@users.noreply.github.com>
2023-03-02 20:30:05 +0900

    js exercise 06 completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -12,5 +12,9 @@ const leapYears = function (year) {
   return true;
 };
 
+// const leapYears = function(year) {
+//   return year % 4 === 0 && ( year % 100 !== 0 || year % 400 === 0);
+// };
+
 // Do not edit below this line
 module.exports = leapYears;
