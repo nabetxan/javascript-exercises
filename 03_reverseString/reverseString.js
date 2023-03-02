f1c234dc44e11c74febf87a59b4c5e99341bbc8d
nabetxan <119922401+nabetxan@users.noreply.github.com>
2023-03-02 20:24:13 +0900

    js exercise 03 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,22 +1,15 @@
-const reverseString = function(str) {
-    return str.split("").reverse().join("");
+const reverseString = function (str) {
+  return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
 module.exports = reverseString;
-
-
-
-
-
 
 // let reverseStringArray = []
 // for (let i = 0; i < str.length; i++){
 //     reverseStringArray.unshift(str[i]);
 // }
 // return reverseStringArray.join("");
-
-
 
 // const reverseString = function(str) {
 //     let reverseString = ""
