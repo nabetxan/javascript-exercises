68b116bfb9b023953a2844cc71e4db868477f8f0
nabetxan <119922401+nabetxan@users.noreply.github.com>
2023-03-02 16:43:06 +0900

    js exercise 11 completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (obj) {
+  const titles = [];
+  obj.map(function (key) {
+    titles.push(key["title"]);
+  });
+  return titles;
 };
 
 // Do not edit below this line
