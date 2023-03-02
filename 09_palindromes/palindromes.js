69fb1357e05be495ffefc995bb884f66d60f50b4
nabetxan <119922401+nabetxan@users.noreply.github.com>
2023-03-02 16:41:11 +0900

    js exercise 09 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (words) {
+  // change to UpperCase
+  let letters = words.toUpperCase();
+  // delete space
+  letters = letters.trim();
+  // delete simpols
+  letters = letters.replace(/[^A-Za-z0–9]/g, "");
 
+  lettersBackwords = "";
+
+  for (let i = letters.length - 1; i >= 0; i--) {
+    lettersBackwords = lettersBackwords + letters[i];
+  }
+  return letters === lettersBackwords;
 };
 
 // Do not edit below this line
