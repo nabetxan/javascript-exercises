9124a843da7a52aaab87234eebd2e23ffad0bd7b
nabetxan <119922401+nabetxan@users.noreply.github.com>
2023-03-08 17:13:47 +0900

    js exercise 08 completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -14,6 +14,10 @@ const sum = function (array) {
   return sumArray;
 };
 
+// const sum = function(array) {
+//   return array.reduce((total, current) => total + current, 0);
+// };
+
 const multiply = function (array) {
   let multiplyArray = 1;
   for (i = 0; i < array.length; i++) {
@@ -22,6 +26,12 @@ const multiply = function (array) {
   return multiplyArray;
 };
 
+// const multiply = function(array) {
+//   return array.length
+//     ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+//     : 0;
+// };
+
 const power = function (num1, num2) {
   let powerNum = num1;
   for (let i = 1; i < num2; i++) {
@@ -29,6 +39,10 @@ const power = function (num1, num2) {
   }
   return powerNum;
 };
+
+// const power = function(a, b) {
+//   return Math.pow(a, b);
+// };
 
 const factorial = function (num) {
   if (num === 0) {
