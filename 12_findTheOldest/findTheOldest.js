b615fea76c1706d1154d3c53eda7c47c2a762695
nabetxan <119922401+nabetxan@users.noreply.github.com>
2023-03-08 17:20:35 +0900

    js exercise 12 completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -30,6 +30,21 @@ const getAge = function (birth, death) {
   return death - birth;
 };
 
+// const findTheOldest = function(array) {
+//   return array.reduce((oldest, currentPerson) => {
+//     const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+//     const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+//     return oldestAge < currentAge ? currentPerson : oldest;
+//   })
+// };
+
+// const getAge = function(birth, death) {
+//   if (!death) {
+//     death = new Date().getFullYear();
+//   }
+//   return death - birth;
+// };
+
 //   let oldestPersonObj = obj[0];
 
 //   for (let i = 1; i < obj.length; i++) {
